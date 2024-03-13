f97c370a4aa74f5e0826963c2da6c9e088e47356
Lucas <hublartlucas@gmail.com>
2024-03-13 09:48:42 +0100

    test

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import Controller.*;
 
 public class Main {
     //Jeu Munchkin
-    // test
+    // test commit
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
 
